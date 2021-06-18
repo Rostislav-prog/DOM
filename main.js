a574c7410491cbe0bc94747a6b77d53338567b75
Rostislav <khmelinin.r@gmail.com>
2021-06-18 10:02:16 +0900

    lesson_6

--- main.js
+++ main.js
@@ -54,16 +54,38 @@ fuckingBoard()
 
 
 
+// Продолжаем реализовывать модуль корзины:
+// Добавлять в объект корзины выбранные товары по клику на кнопке «Купить» без перезагрузки страницы;
+// Привязать к событию покупки товара пересчет корзины и обновление ее внешнего вида.
+
+
+let products = [ // массив - типо товар в магазине
+
+    { id: 556687, name: 'футболка', size: 'm', model: 'hjr5', color: 'blackg', price: 110 },
+    { id: 556686, name: 'носки', size: 'm', model: 'hjr5', color: 'black', price: 200 },
+    { id: 556688, name: 'трусы', size: 'm', model: 'hjr5', color: 'black', price: 500 },
+    { id: 556689, name: 'куртка', size: 'm', model: 'hjr5', color: 'black', price: 10000 },
+
+];
+
+console.log();
+
+function productsSearchForId(i) { // функция выполняет поиск значений в массиве products, если совпадает, отдаёт объект, где хранилось значение
+    for (yy = 0; yy < products.length; yy++) {
+        let obg = products[yy];
+        for (let key in products[yy]) {
+            if (i == obg[key]) {
+                return obg;
+            }
+        }
+    }
+}
+
 
 let htmlBasket = document.getElementById("basket"); // присваеваем блоку с id=basket переменную htmlBasket
 
 
-let basket = [ // массив - типо корзина в магазине
-
-    { name: 'футболка', size: 'm', model: 'hjr5', color: 'black', price: 110 },
-    { name: 'носки', size: 'm', model: 'hjr5', color: 'black', price: 200 },
-    { name: 'трусы', size: 'm', model: 'hjr5', color: 'black', price: 500 },
-    { name: 'куртка', size: 'm', model: 'hjr5', color: 'black', price: 10000 },
+let basket = [ // массив - типо корзина в магазинe
 
 ];
 
@@ -71,19 +93,29 @@ let basket = [ // массив - типо корзина в магазине
 function countBasketPrice(arr) { // функция складывает цены товаров (price) из предоставленной корзины - массива
     let sum = 0;
     for (let num of arr) {
-
         sum += num.price;
-
     }
     return sum; // и отдаёт сумму
-
 }
 
+htmlBasket.innerHTML = "Корзина пуста."
 
-if (countBasketPrice(basket) == 0) { //если сумма цен товаров = 0
-    htmlBasket.innerHTML = "Корзина пуста."
-} else { // иначе 
-    htmlBasket.innerHTML = "В корзине: " + basket.length + " товаров на сумму " + countBasketPrice(basket) + " рублей."; // записываем в перемкнную значение и выводим в html
+let product = document.querySelectorAll('.superclass');
+console.log(product);
+for (i = 0; i < product.length; i++) { // перебор всех блоков с классом superclass
+    product[i].addEventListener('click', function () {
+        let id = (this.id); // находим переменную html блоке 
+        let obgid = productsSearchForId(id); // находим обьект с этой переменной в массиве products
+        console.log(obgid);
+        basket.push(obgid); // добавляем найденный массив в корзину
+        console.log(basket);
+        if (countBasketPrice(basket) == 0) { //если сумма цен товаров = 0
+            htmlBasket.innerHTML = "Корзина пуста."
+        } else { // иначе 
+            htmlBasket.innerHTML = "В корзине: " + basket.length + " товаров на сумму " + countBasketPrice(basket) + " рублей."; // записываем в перемкнную значение и выводим в html
+        }
+
+    })
 }
 
 
